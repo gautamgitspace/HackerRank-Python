949e61d088a9867b95ca177f8a18677543e431ba
Abhishek Gautam <agautam2@buffalo.edu>
2016-06-27 17:27:25 -0400

    intersections

---
+++ Sets/setIntersection.py
@@ -0,0 +1,15 @@
+sEng = set()
+sFr = set()
+count = 0
+subscribersEng = int(raw_input())
+rollNumbersEng = raw_input().split()
+subscribersFr = int(raw_input())
+rollNumbersFr = raw_input().split()
+#print rollNumbersFr
+for engReaders in rollNumbersEng:
+    sEng.add(int(engReaders))
+for frReadrers in rollNumbersFr:
+    sFr.add(int(frReadrers))
+for iterator in (sEng.intersection(sFr)):
+    count = count + 1
+print count
