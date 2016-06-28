3bbb2623781c4af0eb9b1515649f76552824f28b
Abhishek Gautam <agautam2@buffalo.edu>
2016-06-27 20:30:01 -0400

    check subset approach 2

--- Sets/checkSubset.py
+++ Sets/checkSubset.py
@@ -1,10 +1,4 @@
-t = int(raw_input())
-for i in range(t):
-    num_a = int(raw_input())
-    a = set(raw_input().split())
-    num_b = int(raw_input())
-    b = set(raw_input().split())
-    if a.issubset(b):
-        print True
-    else:
-        print False
+for i in range(int(raw_input())):
+    a = int(raw_input()); A = set(raw_input().split()); b = int(raw_input()); B = set(raw_input().split())
+    if A.issubset(B): print True
+    else: print False
