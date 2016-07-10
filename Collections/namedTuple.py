e9ef1aeaef6f3b16a1dc0153c0c8558f266bccef
Abhishek Gautam <agautam2@buffalo.edu>
2016-07-10 17:55:29 -0400

    namedtuple - verbose prints

--- Collections/namedTuple.py
+++ Collections/namedTuple.py
@@ -1,11 +1,13 @@
-#using namedtuple
+#using namedtuple(STU)
 from collections import namedtuple
-count, Student = int(raw_input()), namedtuple('Student', raw_input().split())
+count, STU = int(raw_input()), namedtuple('Student', raw_input().split())
 students = []
 for i in range(count):
     data = raw_input().split()
-    students.append(Student(data[0], data[1], data[2], data[3]))
+    students.append(STU(data[0], data[1], data[2], data[3]))
 sum = 0
-for student in students:
-    sum += float(student.MARKS)
+#print students
+for iterator in students:
+    #print iterator
+    sum += float(iterator.MARKS)
 print (sum / float(count))
