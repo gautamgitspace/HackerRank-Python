d1f86ee7b1f74859425664cd94f58214f2970500
Abhishek Gautam <agautam2@buffalo.edu>
2016-09-27 03:17:30 -0400

    CTCI challenge 20 - lonely integer

---
+++ CTCI/lonelyinteger.py
@@ -0,0 +1,9 @@
+#!/bin/python
+def missing_drone(drones):
+    unique_id = 0
+    for drone in drones:
+        unique_id ^= int(drone)
+    return unique_id
+total_num = int(raw_input())
+drones = raw_input().split()
+print missing_drone(drones)
