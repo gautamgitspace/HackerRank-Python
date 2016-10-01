5e513f24b949b800c5f2eb449e3c811c12b53898
Abhishek Gautam <agautam2@buffalo.edu>
2016-10-01 14:18:09 -0400

    explanation

--- angle_clock_hands.py
+++ angle_clock_hands.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 #angle between the hands of a 12 hour analog clock at any given time in hours and minutes
+#Minute hand turns 6 degrees every minute. Hour hand turns 30 degrees every hour (0.5 degrees every minute)
 def angle(hour, minute):
     angle = abs((hour * 30 + minute * 0.5)- (minute * 6))
     return min(angle, 360-angle)
